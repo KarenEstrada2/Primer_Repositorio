62b4a7bc1247d2221c7cf8160845448d81e1ed39
KarenEstrada2 <karenguadalupeestradacarrera@gmail.com>
2024-10-21 09:09:40 -0600

    Add files via upload

---
+++ P01variables1.php
@@ -0,0 +1,22 @@
+<?php
+/* CBTIS 89
+Karen Estrada
+3°A Programación Matutino
+*/
+$dia = 24; //se declara una variable de tipo integer.
+$sueldo = 758.43; //se declara una variable de tipo double.
+$nombre = " Diana"; //se declara una variable de tipo string.
+$exite = true; //se declara una variable de tipo boolean.
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre;echo "<br>";
+echo "Variable boolean:";
+echo $exite;echo "<br>";
+$cadena1 = "comete un pan";
+$cadena2= "cometelo cometelo, Didi recargame robux, te amo";
+echo $cadena1. " " .$cadena2;
+?>
