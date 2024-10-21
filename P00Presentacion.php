48ca996832476cb84149c35744b8e3b394c27ca2
KarenEstrada2 <karenguadalupeestradacarrera@gmail.com>
2024-10-21 09:29:17 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,17 @@
+
+<?php
+
+/* CBTIS 89
+P00Presentación
+Programa contiene mis datos personales
+Karen Guadalupe Estrada Carrera
+3°A Programacion Matutino
+Durango Dgo.
+*/
+
+echo "mi nombre es Karen Estrada";
+echo "<br>", "Estudio en el CBTIS 89";
+echo "<br>", "En tercer semestre de la carrera de Programación A Matutino";
+echo "<br>", "vivo en la ciudad de Durango Dgo.";
+
+?>
